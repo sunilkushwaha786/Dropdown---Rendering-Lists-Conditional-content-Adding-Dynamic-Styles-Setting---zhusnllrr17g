5e5f17ba82b9151064c5560de300588ef3e31739
sunilkushwaha <sunilkushwaha055>
2023-06-09 18:43:48 +0530

    done

--- src/components/App.js
+++ src/components/App.js
@@ -141,8 +141,59 @@ const states = [{
 function App() 
 {
 	// Do not alter/remove main div
+	const [stateVal, setState] = useState(0);
+	const [cityVal, setCity] = useState(0);
+	const [landVal, setLand] = useState(0);
+
+	const handleState = (e) =>{
+		setState(e.target.value);
+	}
+
+	const handleCity=(e)=>{
+		setCity(e.target.value);
+	};
+
+	const handleLand = (e)=>{
+		setLand(e.target.value);
+	};
+
+
 	return (
 	<div id="main">
+		<select id="state" value={stateVal} onChange={handleState}>
+			{states.map((item, index)=>{
+				return<option value={index}>{item.name}</option>
+			})}
+		</select>
+
+		<select id="city" value={cityVal} onChange={handleCity}>
+			{states[stateVal].city.map((item, index)=>{
+				return<option value={index}>{item.name}</option>
+			})}
+		</select>
+
+		<select id="landmark" value={landVal} onChange={handleLand}>
+			{states[stateVal].city[cityVal].landmarks.map((item, index)=>{
+				return <option value={index}>{item.name}</option>
+			})}
+		</select>
+
+		<div id="state-tittle">{states[stateVal].name}</div>
+		<div id="state-description">{states[stateVal].description}</div>
+		<div id="city-tittle">{states[stateVal].city[cityVal].name}</div>
+
+		<div id="city-description">
+			{states[stateVal].city[cityVal].description}
+		</div>
+
+		<div id="landmark-tittle">
+			{states[stateVal].city[cityVal].landmarks[landVal].name}
+		</div>
+
+		<div id="landmark-description">
+			{states[stateVal].city[cityVal].landmarks[landVal].description}
+		</div>
+
 		
 	</div>
 	);
